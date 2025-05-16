71435f9ec27ebc00123b322bcc1579cb808cdc9b
lperusa7 <lukas.diego.lescano@gmail.com>
2025-05-15 17:15:02 -0700

    changing points to color by latitude

--- test-script.R
+++ test-script.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(lterdatasampler)
-
 ggplot(data = pie_crab,
        aes(x = latitude,
-           y = size)) +
+           y = size,
+           color = latitude)) +
   geom_point()
+scale_color_gradient(low = "#923345",
+                     high = "923403")
